9bcc490c88c6829d501fe6466576bf2d40449ed5
machinedawn <27697763+machinedawn@users.noreply.github.com>
2026-06-12 12:15:42 +0200

    feat: Bump version to 0.5.7 in package.json and clean up unused imports in PlayUnityGameToolTests

--- Assets/Tests/PlayMode/PlayUnityGameToolTests.cs
+++ Assets/Tests/PlayMode/PlayUnityGameToolTests.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Reflection;
-using System.Threading.Tasks;
 using NUnit.Framework;
-using UnityCodeMcpServer.AsyncAwait;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
